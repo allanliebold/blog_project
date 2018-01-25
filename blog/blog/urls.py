112d734cd5dae990c4cbd3badffc876f55abdb57
Allan Liebold <allan.liebold@gmail.com>
2018-01-24 19:00:58 -0800

    linked reset and style css files to posts home page

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -2,9 +2,13 @@
 
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from posts import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
